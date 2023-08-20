c71f2f0837817be52cb309b083dcda25d1b8a04e
glebvi1 <vyazgd@mail.ru>
2023-08-20 16:51:50 +0300

    SQLite for deploy

--- referal_system/settings.py
+++ referal_system/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = 'referal_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "referal_system",
+#         "USER": "glebvi",
+#         "PASSWORD": "1",
+#         "HOST": "localhost",
+#         "PORT": "5433",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "referal_system",
-        "USER": "glebvi",
-        "PASSWORD": "1",
-        "HOST": "localhost",
-        "PORT": "5433",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
